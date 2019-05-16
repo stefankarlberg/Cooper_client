a1e595cf7b8037ca9389a767138e10c3814f31b3
stefan_karlberg <stefan.karlberg@smackit.se>
2019-05-16 10:54:14 +0200

    Added User authentication

---
+++ src/Components/LoginForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const LoginForm = (props) => {
+  return (
+    <form>
+      <div>
+        <label >Email</label>
+        <input id="email" onChange={props.inputChangeHandler}></input>
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input id="password" onChange={props.inputChangeHandler}></input>
+      </div>
+      <button onClick={(e) => props.loginHandler(e)} id="submit">Submit</button>
+    </form>
+  )
+}
+
+export default LoginForm;
