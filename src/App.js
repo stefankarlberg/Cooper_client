2e4e7f15b326828e929fb7e9894e0ccb2af91af6
boa <boa.matule@gmail.com>
2019-05-16 20:55:20 +0200

    Creates a file to DisplayPerformanceData; WIP

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
       email: '',
       password: '',
       message: '',
-      entrySaved: false
+      entrySaved: false,
+      renderIndex: false
     }
   }
   onChange(event) {
@@ -44,12 +45,17 @@ class App extends Component {
   render() {
     let renderLogin;
     let user;
+    let performanceDataIndex;
 
     if (this.state.authenticated === true) {
       user = JSON.parse(sessionStorage.getItem('credentials')).uid;
       renderLogin = (
         <p>Hi {user}</p>
       )
+      performanceDataIndex = (
+        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
+      )
+      
     } else {
       if (this.state.renderLoginForm === true) {
         renderLogin = (
@@ -84,6 +90,7 @@ class App extends Component {
         />  
             
         {renderLogin}
+        {performanceDataIndex}
 
       </div>
     );
